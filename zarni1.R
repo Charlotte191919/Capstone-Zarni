403f012114cfc7b7abeba6d239888d74c7228511
Charlotte Peyton <charlottehudek@charlottesair2.wireless.newschool.edu>
2023-04-19 16:10:56 -0400

    loading analysis data again and changing safety variable

--- zarni1.R
+++ zarni1.R
@@ -1,6 +1,12 @@
 ## first lesson using github 
+library(tidyverse)
+getwd()
 # load data 
+# We wil use the "analysis data" dataset that we created in a previous unit to help 
+# us learn github.
+My_Data <- read_csv("/Users/charlottehudek/Desktop/Capstone-Zarni/analysis_data.csv")
 
-# rename it 
-
-# delete two columns 
+View(My_Data)
+# Let-s rename the variable "safe" to "student_safety" 
+My_Data$student_safety <- My_Data$safe
+# now let's "push" this onto github 
